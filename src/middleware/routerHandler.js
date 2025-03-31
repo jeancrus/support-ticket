a3e29dce464ae2b10bf4a944fd832dcff36c6cd6
Jean Crus <jean.crus@outlook.com>
2025-03-31 20:21:19 -0300

    Adiciona middlewares jsonHandler e routerHandler para manipulação de requisições JSON e roteamento de rotas, respectivamente.

---
+++ src/middleware/routerHandler.js
@@ -0,0 +1,32 @@
+import { Database } from "../database.js";
+import { routes } from "../routes.js";
+import { extractQueryParam } from "../utils/extract-query-param.js";
+
+const database = new Database();
+
+export const routerHandler = (req, res) => {
+  const route = routes.find((route) => {
+    if (route.method === req.method && route.path.test(req.url)) {
+      return true;
+    }
+
+    return false;
+  });
+
+  if (!route) {
+    return res
+      .writeHead(404, {
+        "Content-Type": "text/html",
+      })
+      .end("Rota não encontrada");
+  }
+
+  const routeParams = req.url.match(route.path);
+
+  const { query, ...params } = routeParams.groups;
+
+  req.params = params;
+  req.query = query ? extractQueryParam(query) : {};
+
+  return route.controller({ req, res, database });
+};

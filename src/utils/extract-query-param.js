9f758379527c8399f608d1ac1e4c04eb8eefeea8
Jean Crus <jean.crus@outlook.com>
2025-03-31 20:21:37 -0300

    Adiciona funções utilitárias para extração de parâmetros de consulta e análise de caminhos de rotas.

---
+++ src/utils/extract-query-param.js
@@ -0,0 +1,10 @@
+export const extractQueryParam = (query) => {
+  return query
+    .slice(1)
+    .split("&")
+    .reduce((queryParams, param) => {
+      const [key, value] = param.split("=");
+      queryParams[key] = value;
+      return queryParams;
+    }, {});
+};
